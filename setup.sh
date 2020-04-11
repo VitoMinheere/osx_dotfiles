0b88834adf16b61e0c17ae86d7038bd6abaa137c
Vito Minheere <vitominheere@gmail.com>
2020-04-11 11:43:35 +0200

    Add zshrc and install powerlevel10k in script

--- setup.sh
+++ setup.sh
@@ -101,6 +101,10 @@ pip3 install --user virtualenv
 # Install Oh My Zsh
 [ ! -f ".zshrc" ] && sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+# Install Powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>! ~/.zshrc
+
 # install Flutter
 [ ! -d "flutter" ] && flutter_install
 
